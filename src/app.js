53a857dcc9ab647343d80f89c53b404bb6cee396
Sumit Saha <sumit360saha@gmail.com>
2025-08-16 23:51:23 +0530

    Initiated a Express Working Server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+/*
+- package.json helps to manage the dependencies and scripts for the project, if node_modules is not present, it can be installed using `npm install`
+- .gitignore is used to specify files and directories that should not be tracked by git,
+- in this case, it is used to ignore the node_modules directory.
+- app.js is the main entry point of the application, it sets up an Express server and
+- defines a route that responds to requests made to `/index`.
+- The server listens on port 3003 and responds with a message indicating the request URL and port number.
+- This code is part of a Node.js application that uses Express to create a simple server.
+*/
+
+import express from "express";
+
+const app = express();
+
+app.use("/index", (req, res, next) => {
+    // res.send("Server is live!!")
+    res.send(`Request has been made to: ${req.url} ${server.address().port}`);
+    // next();
+})
+
+const server = app.listen(3003, () => {
+    console.log(`Server is started successfully on port ${server.address().port}`);
+});
