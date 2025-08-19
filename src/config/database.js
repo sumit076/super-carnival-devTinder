49ab9095a52225642e2a0cc49bc7557c14c9a567
Sumit Saha <Sumit360saha@gmail.com>
2025-08-20 02:08:20 +0530

    Update database.js
    Updated the Access

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export default class databaseConn {
 
     constructor() {
-        process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Cluster0:yHqkRd84ONvgew02@cluster0.hvrh4t8.mongodb.net/devTinder';
+        process.env.MONGO_URI = process.env.MONGO_URI; // Provide the cluster / db uri to connect to the database
     }
 
     async connectDB() {
