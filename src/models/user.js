8b846c3de3609ee9f4a536628c8290cd686b1508
Sumit Saha <sumit360saha@gmail.com>
2025-09-07 17:37:02 +0530

    Added backend restrictions,default values  and validations to the User Schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 export default class userSchema {
     constructor() {
@@ -10,17 +11,28 @@ export default class userSchema {
             email: {
                 type: String,
                 required: true,
-                unique: true
+                unique: true,
+                validate(value) {
+                    if (!(['gmail.com', 'outlook.com', 'rediffmail.com'].includes(value.split('@')[1]))) {
+                        throw new Error("Email domain must be one of gmail.com, outlook.com, rediffmail.com");
+                    }
+                }
             },
             password: {
                 type: String,
-                required: true
+                required: true,
+                default: "password1234",
+                validate(value) {
+                    if (!validator.isStrongPassword(value, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 })) {
+                        throw new Error("Password is not strong enough. It should be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one symbol.");
+                    }
+                }
             },
             isAdmin: {
                 type: Boolean,
                 default: false
             }
-        });
+        }, { timestamps: true });
     }
 
     getSchema() {
